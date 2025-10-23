c21445d25200c5fdb5b1941110ddaca75a07d2b8
rhodaappiah4 <nananokyewaa.appiah@gmail.com>
2025-10-23 07:50:14 -0400

    Updating logic in routes. Tasks 1 through to Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,26 +12,52 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify({books},null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+   // Retrieve the isbn parameter from the request URL and send the corresponding book details
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Retrieve the author parameter from the request URL and send the corresponding book details
+    const author = req.params.author.replace('-', ' ');
+    const matchingBooks = [];
+
+    for (let key in books) {    
+        if (books[key].author.toLowerCase() === author.toLowerCase()) {
+            matchingBooks.push(books[key]);
+        }
+    }
+    if (matchingBooks.length > 0){
+        res.send(matchingBooks);
+    }
+    else{
+        return res.status(404).send({ message: 'Author not found' });
+    } 
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Retrieve the title parameter from the request URL and send the corresponding book details
+    const title = req.params.title.replace('-', ' ');
+    const matchingBooks = [];
+
+    for (let key in books) {    
+        if (books[key].title.toLowerCase() === title.toLowerCase()) {
+            matchingBooks.push(books[key]);
+        }
+    }
+    if (matchingBooks.length > 0){
+        res.send(matchingBooks);
+    }
+    else{
+        return res.status(404).send({ message: 'Title not found' });
+    } 
 });
 
 //  Get book review
